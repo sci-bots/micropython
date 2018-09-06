683fc85b9099226ded9daed2226bcc4fb6e357a1
Christian Fobel <christian@fobel.net>
2018-09-06 18:01:02 -0400

    feat(teensy/asyncio): add fast_io asyncio module
     - Add `fast_io` `asyncio` module
     - Update `main.py` with example on how to use `uasyncio`
    
    Known issue
    -----------
    
    Importing `asyn` takes ~20 kB, but this memory can be freed after
    importing using `gc.collect()`.  See `ports/teensy/memzip_files/main.py`
    for an example.

--- ports/teensy/memzip_files/main.py
+++ ports/teensy/memzip_files/main.py
@@ -1,15 +1,31 @@
+import gc
+gc.collect()
+import asyn
+gc.collect()
+import uasyncio as asyncio
+gc.collect()
 import pyb
 
-print("Executing main.py")
 
-led = pyb.LED(1)
-
-led.on()
-pyb.delay(100)
-led.off()
-pyb.delay(100)
-led.on()
-pyb.delay(100)
-led.off()
+async def foo(event, pin):
+    pin.value(1)
+    print('hello, foo!')
+    await event
+    print('hello, foo, again!')
+    pin.value(0)
+    event.clear()
+    return 'foo'
 
 
+async def bar(event):
+    print('hello, bar!')
+    await asyncio.sleep(2)
+    event.set()
+    print('bar finished!')
+    return 'bar'
+
+loop = asyncio.get_event_loop()
+pin = pyb.Pin('D13', mode=pyb.Pin.OUT)
+event = asyn.Event()
+task = asyn.Gather([asyn.Gatherable(foo, event, pin), asyn.Gatherable(bar, event)])
+loop.run_until_complete(task)
